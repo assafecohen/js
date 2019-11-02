3e8723a10728d7f61dea797fd7b7d5178d8715d8
Assafm <assafe.cohen@gmail.com>
2019-11-03 00:35:46 +0200

    added insert book functionality

---
+++ book_list_project/app.js
@@ -0,0 +1,52 @@
+//Book contructor
+function Book(title, author, isbn){
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+//UI contructor
+function UI(){
+  // add book to list
+ UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+    //Create  tr element
+    const row = document.createElement('tr');
+    // insert cols 
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+    `;
+    list.appendChild(row);
+ }
+ UI.prototype.clearFields = function(){
+   document.getElementById('title').value = '';
+   document.getElementById('author').value = '';
+   document.getElementById('isbn').value = '';
+ }
+}
+
+
+//Event Listeners
+document.getElementById('book-form').addEventListener('submit',
+  function(e){
+    // get form values
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const isbn = document.getElementById("isbn").value;
+
+    // Instantiate book
+   
+    const book = new Book (title,author, isbn);
+    // Instantiate UI
+    const ui = new UI();
+
+    //add book to list
+    ui.addBookToList(book);
+
+    //Clear Fields
+    ui.clearFields();
+    e.preventDefault();
+});
