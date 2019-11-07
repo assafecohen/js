e57a5763c7ac69b3e65ac586a9936f1b7a1095fa
Assafm <assafe.cohen@gmail.com>
2019-11-07 23:23:36 +0200

    update

--- Asynchronous/ezhttp/app.js
+++ Asynchronous/ezhttp/app.js
@@ -1,70 +1,52 @@
 const http = new easyHTTP;
-const data = {
-  title: "Custome Post",
-  body: "this is a custom post"
-};
-// // Get posts
-// http.get('https://jsonplaceholder.typicode.com/posts',
-//   function (err, posts) {
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       console.log(posts);
-//     }
-//   });
 
-// console.log(posts);
-
-// // Get single Post
-// http.get('https://jsonplaceholder.typicode.com/posts/1',
-//   function (err, posts) {
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       console.log(posts);
-//     }
-//   });
-
-// console.log(posts);
-
-
-// // Create Data
-
-
-
-// // Create Post
-// http.post('https://jsonplaceholder.typicode.com/posts', data,
-//   function (err, post) {
-//     if (err) {
-//       console.log(err)
-
-//     } else {
-//       console.log(post);
-
-//     }
-//   });
-
-// // Update data
-
-http.put('https://jsonplaceholder.typicode.com/posts/1',
-  data, function (err, post) {
-    if (err) {
-      console.log(err)
-
-    } else {
-      console.log(post);
-    }
-  });
-
-
-// delete
-//  http.delete('https://jsonplaceholder.typicode.com/posts/1',
-//  function(err ,posts){
-//    if(err){
-//      console.log(err)
-//    }else{
-//      console.log(posts);
-//    }
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
 // });
 
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
 
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+});
